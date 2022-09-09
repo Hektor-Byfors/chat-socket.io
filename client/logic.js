03cfee0438fcab8ad23e6195f14898236c17b38b
Hektor-Byfors <hektor.byfors@hotmail.com>
2022-09-09 17:06:29 +0200

    socket connections

--- client/logic.js
+++ client/logic.js
@@ -1,11 +1,9 @@
-let socket = io({ "autoconnect":false});
+let socket = io({"autoConnect": false});
 
 let nickname = "";
 let room = "";
 
-
-
-window.addEventListener("load", () => {
+const welcomePage = () => {
     let container = document.createElement("div");
     container.classList.add("welcomeBox");
 
@@ -21,12 +19,81 @@ window.addEventListener("load", () => {
     let welcomeButton = document.createElement("button");
     welcomeButton.innerText = "Continue"
     welcomeButton.classList.add("welcomeButton");
-    welcomeButton.addEventListener("click", () => {
-        if(nickname != "") {
-            console.log(nickname);
-        }
-    })
+    welcomeButton.addEventListener("click", welcomePageButton)
 
     document.body.append(container);
     container.append(welcomeHeader, nameInput, welcomeButton)
-})
+};
+
+const welcomePageButton = () => {
+    if(nickname === "") return
+
+    socket.connect();
+
+    socket.emit("nickname", nickname);
+
+    document.body.innerHTML = "";
+
+    let mainContainer = document.createElement("div");
+    mainContainer.classList.add("mainContainer");
+
+    let leftContainer = document.createElement("div");
+    leftContainer.classList.add("leftContainer");
+
+    let rightContainer = document.createElement("div");
+    rightContainer.classList.add("rightContainer");
+
+    let welcomeUserText = document.createElement("h2");
+    welcomeUserText.classList.add("welcomeUserText");
+    welcomeUserText.innerHTML = `Welcome </br> ${nickname}`
+
+    let roomInputBox = document.createElement("input");
+    roomInputBox.addEventListener("input", (e) => {
+        room = e.target.value;
+    });
+    
+    let roomButton = document.createElement("button");
+    roomButton.innerText = "Join room";
+    roomButton.addEventListener("click", () => {
+        if(room != ""){
+            socket.emit("room", room);
+        }
+    })
+
+    let chatMsgBox = document.createElement("div");
+    chatMsgBox.classList.add("chatMsgBox");
+
+    let chatInputBox = document.createElement("div");
+    chatInputBox.classList.add("chatInputBox");
+
+    let chatInput = document.createElement("input");
+
+    let chatButton = document.createElement("button");
+    chatButton.innerText = "Send";
+    chatButton.addEventListener("click", () => {
+        if(chatInput.value != ""){
+            let chatMsgdiv = document.createElement("div");
+
+            let chatMsgSender = document.createElement("h5");
+            chatMsgSender.innerText = nickname;
+
+            let chatMsg = document.createElement("p");
+            chatMsg.classList.add("chatMsg");
+            chatMsg.innerText = chatInput.value
+
+            chatMsgBox.append(chatMsgdiv);
+            chatMsgdiv.append(chatMsgSender, chatMsg); 
+        }
+
+        
+
+});
+
+    document.body.append(mainContainer);
+    mainContainer.append(leftContainer, rightContainer);
+    leftContainer.append(welcomeUserText, roomInputBox, roomButton);
+    rightContainer.append(chatMsgBox, chatInputBox)
+    chatInputBox.append(chatInput, chatButton);
+}
+
+window.addEventListener("load", welcomePage);
--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const io = require("socket.io")(server);
 app.use("/", express.static("./client"));
 
 io.on("connection", (socket) => {
-    console.log("connected socket");
 
     socket.on("disconnect", () => {
         console.log("user disconnected");
+    })
+
+    socket.on("nickname", data => {
+        console.log("user " + data);
+    })
+
+    socket.on("room", data => {
+        //console.log("room join " + data);
+        socket.join(data);
+        //console.log(io.sockets.adapter.rooms );
     })
 
 })
